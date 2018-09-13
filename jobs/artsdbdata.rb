8c83984c06c5d267274a840fbf16d37f6d6eb048
Sol De Tres <soldetres@gmail.com>
2018-09-13 06:52:00 -0400

    Adding other TestGroups with different outcomes.

--- jobs/artsdbdata.rb
+++ jobs/artsdbdata.rb
@@ -9,12 +9,15 @@ SCHEDULER.every '2s' do |job|
  # begin
   #build_url = 'validate.jsontest.com/?json={key : value}'
   #build_url = 'numbersapi.com/random/date?json' 
-  build_url = 'echo.jsontest.com/TestGroup1/0/TestGroup2/1/TestGroup3/21' 
+  build_url = 'echo.jsontest.com/TestGroup1/1/TestGroup2/0/TestGroup3/21/TestGroup4/-1/TestGroup5/--'
+
   response = RestClient.get build_url, { accept: :json }
   data = JSON.parse(response.body)
   send_event('TestGroup1', {value: data['TestGroup1'],value2: rand(100) })
   send_event('TestGroup2', {value: data['TestGroup2'],value2: rand(100)})
   send_event('TestGroup3', {value: data['TestGroup3'],value2: rand(100)})
+  send_event('TestGroup4', {value: data['TestGroup4'],value2: rand(100)})
+  send_event('TestGroup5', {value: data['TestGroup5'],value2: '--'})
 
 
 end
